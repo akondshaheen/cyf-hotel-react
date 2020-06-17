ffc2686fb2892f9d7414c34f50463360a135fad4
akondshaheen <akondshaheen.es@gmail.com>
2020-06-17 11:03:30 +0200

    Fix error

--- src/Compnents/orders/order.js
+++ src/Compnents/orders/order.js
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 import RestaurantButton from "./RestaurantButton";
 
-const Order = () => {
+const Order = prop => {
   const [orders, setOrders] = useState(0);
 
-  const orderType = ["Salad", "Pizzas"];
-
   function orderOne() {
-    return setOrders(count => count + 1);
+    return setOrders(orders + 1);
   }
   return (
     <div>
-      <li>
-        {orderType[1]}: {orders}
-      </li>
-      <br />
-      <li>
-        {orderType[0]}: {orders}
+      <li className="orderAlign">
+        {prop.orderType}: {orders}
+        <RestaurantButton orderOne={orderOne} />
       </li>
     </div>
   );
